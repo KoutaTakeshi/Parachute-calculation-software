813ef1903c801496cfe444bf25b98819f633bbd6
Shampoo <353079908@qq.com>
2022-03-22 11:48:31 +0800

    Inflation Complete
    Finish the inflation program, now it can output figures about Fk(opening shock), s, v, CA and mf 充气阶段编程完成，现在可以输出开伞动载，位移，速度，阻力特征和附加质量图像

--- chongqi.m
+++ chongqi.m
@@ -1,60 +1,35 @@
 function dv=chongqi(t,v)
-    
     global den g ms mw vL kf;
     
     %密织物平面圆形伞plane circular
-    D0=5; %伞衣名义直径
+    D0=8; %伞衣名义直径
     C=0.8; %阻力系数
-    kd=2; %开伞动载系数
     lambda=1.74; %充气距离比例系数λ
     alpha=6; %Scheubel常数
     scale=0.04; %(CA)1/(CA)s
 
     %无收口non-close
+    global tm;
     t1=lambda*D0/vL; %初始充气时间
-    tm=alpha*D0/vL; %充满时间（缺少速度修正系数kv）
+    tm=alpha*D0/(0.9*vL); %充满时间(kv取0.9)
     kt=scale*C*(pi*(D0/2)^2)/t1;
     n=2;
     betat=(1-scale)*C*(pi*(D0/2)^2)/((tm-t1)^n); %n取2
-    %mf=den*kf*(ncCA)^1.5; %附加质量
       
-    dv=zeros(2,1); %2或3
+    dv=zeros(4,1); 
     if t<=t1
-       dv(1)=kt; %dCA/dt
+        dv(1)=kt; %dCA/dt
+    elseif t<=tm
+        dv(1)=n*betat*(t-t1)^(n-1);
     else
-       dv(1)=n*betat*(t-t1)^(n-1);
+        dv(1)=0;
     end
     dv(2)=1.5*den*kf*(v(1)^0.5)*dv(1); %dmf/dt
-    dv(3)=-(mw+ms)*g/(mw+ms+v(2))-0.5*den*(v(3)^2)*v(1)/(mw+ms+v(2))-v(3)*dv(2)/(mw+ms+v(2)); %dv/dt
-    v(4)=(ms+v(2))*dv(3)+0.5*den*(v(3)^2)*v(1)+v(3)*dv(2)+ms*g; %Fk
-    dv(5)=v(3); %ds/dt
-%     if t<=t1
-%         v(1)=kt*t;
-%     else
-%         v(1)=scale*C*(pi*(D0/2)^2)+betat*(t-t1)^2; %n取2
-%     end
-%     v(2)=den*kf*(v(1))^1.5; %mf
-%     a=(mw+ms)/(mw+ms+v(2));
-%     b=v(1)/(mw+ms+v(2)); %缺(CA)w
-%     c=1/(mw+ms+v(2));
-%     dv(3)=-a*g-0.5*den*v(2)*b-c*v(3)*dv(2); %dv/dt
-%     dv(4)=v(3); %ds/dt
-%     a=(mw+ms)/(mw+ms+den*kf*(CA(t))^1.5);
-%     b=CA(t)/(mw+ms+den*kf*(CA(t))^1.5); %缺(CA)w
-%     c=1/(mw+ms+den*kf*(CA(t))^1.5);
-%     dv(1)=-a*g-0.5*den*CA(t)*b-c*v(1)*dCA(t); %dv/dt
-%     dv(2)=v(1); %ds/dt
+    dv(3)=(mw+ms)*g*sind(v(5))/(mw+ms+v(2))-0.5*den*(v(3)^2)*v(1)/(mw+ms+v(2))-v(3)*dv(2)/(mw+ms+v(2)); %dv/dt,缺(CA)w
+    dv(4)=v(3); %ds/dt
+    dv(5)=g*cosd(v(5))/v(3); %d(theta)/dt
+    dv(6)=v(3)*cosd(v(5)); %dx/dt
+    dv(7)=-v(3)*sind(v(5)); %dy/dt
 
 end
 
-%伞阻力特征CA
-% function a=CA(t)
-% 
-%     global t1 kt scale C D0 betat 
-%     if t<=t1
-%         a=kt*t;
-%     else
-%         a=scale*C*(pi*(D0/2)^2)+betat*(t-t1)^2; %n取2
-%     end
-%     
-% end
--- mysphere.m
+++ mysphere.m
@@ -1,18 +1,64 @@
-global den g ms mw vL kf;
+clc;
+clear;
+global den g ms mw vL hL kf tm theta;
 den=1.205; %空气密度
 g=9.8;
-ms=1; %降落伞质量
-mw=1; %物体质量
+ms=5; %降落伞质量
+mw=70; %物体质量
 vL=50; %拉直速度
+hL=1500; %拉直阶段结束高度
 kf=0.41;
- 
+theta=82; %倾斜角
+
 %常微分方程求解(不考虑倾斜角)
-tspan=[0 100];
+tspan=(0:0.01:tm+2); %求解时间范围
 
-[t,v]=ode45('chongqi',tspan,[0 0 vL 0 0]);
+[t,v]=ode45('chongqi',tspan,[0 0 vL 0 theta 0 hL]);
 
-plot(t,v(:,4)); %v图像
-%plot(t,s);
+hC=hL-v(1:length(t)-1,4); %充气阶段高度变化
+x=(ms+v(1:length(t)-1,2)).*diff(v(:,3))./diff(t);
+y=0.5*den*(v(1:length(t)-1,3).^2).*v(1:length(t)-1,1);
+z=v(1:length(t)-1,3).*diff(v(:,2))./diff(t);
+Fk=x+y+z-ms*g*sind(v(1:length(t)-1,5));
+
+figure(1);
+
+subplot(2,4,1);
+plot(t,v(:,1)); 
+title("CA");
+
+subplot(2,4,2);
+plot(t,v(:,2));
+title("mf");
+
+subplot(2,4,3);
+plot(t,v(:,3));
+title("v");
+
+subplot(2,4,4);
+plot(t,v(:,4));
+title("s"); %位移
+
+subplot(2,4,5);
+plot(t,v(:,5));
+title("theta");
+
+subplot(2,4,6);
+plot(t,v(:,6));
+title("x"); %水平方向位移
+
+subplot(2,4,7);
+plot(t,v(:,7));
+title("hC"); %竖直方向位移
+
+
+figure(2);
+
+plot(t(1:length(t)-1,:),Fk);
+title("Fk");
+
+
+
 
 
 
