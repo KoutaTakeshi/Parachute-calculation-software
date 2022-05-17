85a5b19f3b05d6ee6e2e7d029b3a9f3aec4fd504
DoraShin <353079908@qq.com>
2022-05-17 20:49:19 +0800

    finish

--- inflation.m
+++ inflation.m
@@ -1,35 +1,33 @@
-function dv=inflation(t,v)
-    global den g ms mw vL kf t1;
+function dv = inflation(t, v)
+    global den g ms mw kf tm;
+    global kt betat n ti sk ts;
     
-    %密织物平面圆形伞plane circular
-    D0=8; %伞衣名义直径
-    C=0.8; %阻力系数
-    lambda=1.74; %充气距离比例系数λ
-    alpha=6; %Scheubel常数
-    scale=0.04; %(CA)1/(CA)s
-
-    %无收口non-close
-    global tm;
-    t1=lambda*D0/vL; %初始充气时间
-    tm=alpha*D0/(0.9*vL); %充满时间(kv取0.9)
-    kt=scale*C*(pi*(D0/2)^2)/t1;
-    n=2;
-    betat=(1-scale)*C*(pi*(D0/2)^2)/((tm-t1)^n); %n取2
-      
-    dv=zeros(4,1); 
-    if t<=t1
-        dv(1)=kt; %dCA/dt
-    elseif t<=tm
-        dv(1)=n*betat*(t-t1)^(n-1);
-    else
-        dv(1)=0;
+    dv = zeros(7, 1);
+    if sk == 0 %无收口
+        if t <= ti
+            dv(1) = kt; %dCA / dt
+        elseif t <= tm
+            dv(1) = n * betat * (t - ti).^(n - 1);
+        else
+            dv(1) = 0;
+        end
+    else %一次收口
+        if t <= ti
+            dv(1) = kt; %dCA / dt
+        elseif t <= (ti + ts)
+            dv(1) = 0;
+        elseif t <= (ti + ts + tm)
+            dv(1) = 2 * betat * (t - ti - ts);
+        else
+            dv(1) = 0;
+        end
     end
-    dv(2)=1.5*den*kf*(v(1)^0.5)*dv(1); %dmf/dt
-    dv(3)=(mw+ms)*g*sind(v(5))/(mw+ms+v(2))-0.5*den*(v(3)^2)*v(1)/(mw+ms+v(2))-v(3)*dv(2)/(mw+ms+v(2)); %dv/dt,缺(CA)w
-    dv(4)=v(3); %ds/dt
-    dv(5)=g*cosd(v(5))/v(3); %d(theta)/dt
-    dv(6)=v(3)*cosd(v(5)); %dx/dt
-    dv(7)=-v(3)*sind(v(5)); %dy/dt
+    dv(2) = 1.5 * den * kf * (v(1)^0.5) * dv(1); %dmf / dt
+    dv(3) = (mw + ms) * g * sind(v(5)) / (mw + ms + v(2)) - 0.5 * den * (v(3)^2) * v(1) / (mw + ms + v(2))...
+                 - v(3) * dv(2) / (mw + ms + v(2)); %dv / dt, 忽略(CA)w
+    dv(4) = v(3); %ds / dt
+    dv(5) = (180 / pi) * g * cosd(v(5)) / v(3); %d(theta) / dt
+    dv(6) = v(3) * cosd(v(5)); %dx / dt
+    dv(7) =  - v(3) * sind(v(5)); %dh / dt
 
 end
-
--- main.m
+++ main.m
@@ -1,65 +1,74 @@
-clc;
-clear;
-global den g ms mw vL hL kf tm theta;
-den=1.205; %空气密度
-g=9.8;
-ms=5; %降落伞质量
-mw=70; %物体质量
-vL=50; %拉直速度
-hL=1500; %拉直阶段结束高度
-kf=0.41;
-theta=82; %倾斜角
+global den g ms msh msy vZ hZ tm kf theta;
+global C D0 A0 densh densy densystar Lsh Lxt b;
+global kt betat n ti;
+global t2 v2 Fk t2n v2n dtrailx dtraily itrailx itraily;
+global sk FL me tmax;
 
-%常微分方程求解(不考虑倾斜角)
-tspan=(0:0.01:tm+2); %求解时间范围
+me =0;
+g = 9.8;
+kf = 0.41; %附加质量系数kf = 0.752 / Ct^1.5
+A0 = pi * (D0 / 2)^2; %伞衣面积
+b = D0 / 100; %伞衣底边宽度
+Lxt = Lsh + D0 / 2; %伞系统长度
+msh = densh * Lsh; %伞绳质量
+msy = densystar * b + densy * (D0 / 2 - b); %伞衣质量
+ms = msh + msy; %降落伞质量
 
-[t,v]=ode45('inflation',tspan,[0 0 vL 0 theta 0 hL]);
+%% 拉直过程
+tspan1 = (0 : 0.01 : 2); %求解时间范围
+[t1, v1] = ode45('deployment', tspan1, [0 0 vZ vZ theta 0 hZ]);
 
-hC=hL-v(1:length(t)-1,4); %充气阶段高度变化
-x=(ms+v(1:length(t)-1,2)).*diff(v(:,3))./diff(t);
-y=0.5*den*(v(1:length(t)-1,3).^2).*v(1:length(t)-1,1);
-z=v(1:length(t)-1,3).*diff(v(:,2))./diff(t);
-Fk=x+y+z-ms*g*sind(v(1:length(t)-1,5));
+%拉直力求解
+tmax = 1;
+while v1(tmax, 2) < Lxt
+    tmax = tmax + 1;
+end
+FL = zeros([1, 1]);
+for i = 1 : tmax - 1
+    if v1(i, 2) < Lsh
+        FL(i, 1) = densh * (v1(i, 3) - v1(i, 4))^2;
+    elseif v1(i, 2) < Lxt
+        FL(i, 1) = densy * (Lxt - v1(i, 2)) * (v1(i, 3) - v1(i, 4))^2; %伞衣单位长度质量
+    end
+end
+dtrailx = [v1(1 : tmax - 1, 6) + v1(1 : tmax - 1, 2) .* cosd(v1(1 : tmax - 1, 5)), v1(1 : tmax - 1, 6)]; 
+dtraily = [v1(1 : tmax - 1, 7) + v1(1 : tmax - 1, 2) .* sind(v1(1 : tmax - 1, 5)), v1(1 : tmax - 1, 7)];
+v1(tmax, 4) = v1(tmax - 1, 3); %全长拉直时刻，引导伞速度突变为载荷速度
 
-figure(1);
+%% 充气过程
+lambda = 1.74; %充气距离比例系数λ
+lambda1 = 14;
+lambdat = 4.42;
+alpha = 6; %Scheubel常数
+scale = 0.04; %(CA)1 / (CA)
+vjsk = 30; %解除收口时物伞系统速度
+n = 2;
+if sk == 0
+    %无收口
+    ti = lambda * D0 / v1(tmax - 1, 3); %初始充气时间
+    tm = alpha * D0 / (0.9 * v1(tmax - 1, 3)); %充满时间(kv取0.9)
+    kt = scale * C * A0 / ti;
+    betat = (1 - scale) * C * A0 / ((tm - ti)^n); %n取2
+else
+    %一次收口
+    ti = lambda1 * scalesk * D0 / v1(tmax - 1, 3); %第一段充气时间
+    tm = lambdat * (1 - scalesk) * (C * A0)^0.5 / vjsk; %充满时间
+    kt = scalesk^2 * C * A0 / ti;
+    betat = (C * A0 - kt * ti) / tm;
+end
 
-subplot(2,4,1);
-plot(t,v(:,1)); 
-title("CA");
+tspan2 = (0 : 0.01 : tm + 2.5);
+[t2, v2] = ode45('inflation', tspan2, [0 0 v1(tmax - 1, 3) 0 v1(tmax - 1, 5) v1(tmax - 1, 6) v1(tmax - 1, 7)]);
+[t2n, ~] = size(t2);
+[v2n, ~] = size(v2);
+itrailx = [v2(:, 6) + Lxt * cosd(v2(:, 5)), v2(:, 6)]; 
+itraily = [v2(:, 7) + Lxt * sind(v2(:, 5)), v2(:, 7)];
 
-subplot(2,4,2);
-plot(t,v(:,2));
-title("mf");
-
-subplot(2,4,3);
-plot(t,v(:,3));
-title("v");
-
-subplot(2,4,4);
-plot(t,v(:,4));
-title("s"); %位移
-
-subplot(2,4,5);
-plot(t,v(:,5));
-title("theta");
-
-subplot(2,4,6);
-plot(t,v(:,6));
-title("x"); %水平方向位移
-
-subplot(2,4,7);
-plot(t,v(:,7));
-title("hC"); %竖直方向位移
-
-
-figure(2);
-
-plot(t(1:length(t)-1,:),Fk);
-title("Fk");
-
-
-
-
-
-
+%开伞动载求解
+a1 = (ms + v2(1 : length(t2) - 1, 2)) .* diff(v2( : , 3)) ./ diff(t2);
+a2 = 0.5 * den * (v2(1 : length(t2), 3).^2) .* v2(1 : length(t2), 1);
+a3 = v2(1 : length(t2) - 1, 3) .* diff(v2( : , 2)) ./ diff(t2);
+a1(length(t2)) =  a1(length(t2) - 1);
+a3(length(t2)) = a3(length(t2) - 1);
+Fk = a1 + a2 + a3 - ms * g * sind(v2(1 : length(t2), 5));
 
